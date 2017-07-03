e2fc91bcf1a3a47145a72beed90a73b42940646a
US\sdutta <sdutta@deloitte.com>
2017-07-03 22:32:23 +0530

    no message

---
+++ Services/Controllers/ContentController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace Services.Controllers
+{
+    public class ContentController : ApiController
+    {
+        // GET api/values 
+        [HttpGet]
+        [Route("", Name = "Get")]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5 
+        [HttpGet]
+        [Route("{id}", Name = "GetById")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+        
+    }
+}
---
+++ Services/Startup.cs
@@ -0,0 +1,37 @@
+﻿using System.Web.Http;
+using Owin;
+
+namespace Services
+{
+    public static class Startup
+    {
+        // This code configures Web API. The Startup class is specified as a type
+        // parameter in the WebApp.Start method.
+        public static void ConfigureApp(IAppBuilder appBuilder)
+        {
+            // Configure Web API for self-host. 
+            //HttpConfiguration config = new HttpConfiguration();
+
+            //config.Routes.MapHttpRoute(
+            //    name: "DefaultApi",
+            //    routeTemplate: "api/{controller}/{id}",
+            //    defaults: new { id = RouteParameter.Optional }
+            //);
+
+            //appBuilder.UseWebApi(config);
+
+            HttpConfiguration config = new HttpConfiguration();
+
+            //config.Filters.Add(new LogRequest());
+            //RegisterExceptionFilter(config);
+
+            config.MapHttpAttributeRoutes();
+
+            //SwaggerConfig.Register(config);
+            //app.UseCors(CorsOptions.AllowAll);
+
+            //config.MessageHandlers.Insert(0, new CompressionHandler());
+            appBuilder.UseWebApi(config);
+        }
+    }
+}
